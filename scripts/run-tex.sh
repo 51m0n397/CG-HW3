afda0ae52acc4328aa6ed5b9993a6ebff3278968
51m0n397 <simbartolini@gmail.com>
2021-01-07 19:26:56 +0100

    added texture test

---
+++ scripts/run-tex.sh
@@ -0,0 +1,2 @@
+./bin/ypathtrace tests/random_tex/scene.json -o out/tex/random_tex.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/no_random_tex/scene.json -o out/tex/no_random_tex.jpg -t volpath -s 256 -r 720
