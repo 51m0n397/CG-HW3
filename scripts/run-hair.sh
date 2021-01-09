7da2483eeb56af3d04ea2fb87ce75703ea9f795f
51m0n397 <simbartolini@gmail.com>
2021-01-09 11:35:19 +0100

    changed test scripts

---
+++ scripts/run-hair.sh
@@ -0,0 +1,10 @@
+./bin/ypathtrace tests/hair/straight-hair/scene.pbrt -o out/hair/straight-hair.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/curly-hair/scene.pbrt -o out/hair/curly-hair.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/hair-curls/scene.pbrt -o out/hair/hair-curls.jpg -t volpath -s 256 -r 720
+
+./bin/ypathtrace tests/hair/hair_sigma_a/hair_sigma_a.json -o out/hair/hair_sigma_a.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/hair_melanin/hair_melanin.json -o out/hair/hair_melanin.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/hair_color/hair_color.json -o out/hair/hair_color.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/hair_beta_m/hair_beta_m.json -o out/hair/hair_beta_m.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/hair_beta_n/hair_beta_n.json -o out/hair/hair_beta_n.jpg -t volpath -s 256 -r 720
+./bin/ypathtrace tests/hair/hair_alpha/hair_alpha.json -o out/hair/hair_alpha.jpg -t volpath -s 256 -r 720
